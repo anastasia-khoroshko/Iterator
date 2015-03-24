b840c8c768a60ff65a5e40cbe687107dff400c6f
anastasia-khoroshko <anastasia.khoroshko@gmail.com>
2015-03-24 20:37:19 +0300

    Iterator
    Implement

---
+++ Iterator/ExtensionMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public static class ExtensionMethod
+    {
+        public static IEnumerable<T> Iterator<T>(this IEnumerable<T> list, Func<T, bool> comparer)
+        {
+            if (Object.ReferenceEquals(list, null) || Object.ReferenceEquals(comparer, null)) throw new ArgumentNullException();
+            foreach (T elem in list)
+            {
+                if (comparer(elem))
+                    yield return elem;
+            }
+        }
+
+        public static IEnumerable<T> Iterator<T>(this IEnumerable<T> list,int from=0,int to=100,Func<int,int> comparer)
+        {
+            if (Object.ReferenceEquals(list, null) || Object.ReferenceEquals(comparer, null)) throw new ArgumentNullException();
+            T[] a=list.ToArray<T>();
+            for(int i=from;i<=to;i++)
+            {
+                if (to > a.Length||to<from) throw new IndexOutOfRangeException();
+                yield return a[i];
+                i = comparer(i);
+            }
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1,2,3,4};
+            foreach(int elem in ExtensionMethod.Iterator(list,x=>x%2==1))
+            {
+                Console.WriteLine(elem);
+            }
+            var list1 = new List<char> { 'a','b', 'c', 'd' };
+            foreach (char elem in ExtensionMethod.Iterator(list1, x => x.CompareTo('a')==0))
+            {
+                Console.WriteLine(elem);
+            }
+            var list2 = new List<double> {1.2,2.5,6.3,7.7,2.6,1.0 };
+            foreach (double elem in ExtensionMethod.Iterator(list2, 1, 6, x => { return x + 1; }))
+            {
+                Console.WriteLine(elem);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
